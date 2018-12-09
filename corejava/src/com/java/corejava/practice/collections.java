37a7b19e0ea8fa925501215bf548eecb26e75ab0
syamdineshb <syamdinesh@gmail.com>
2018-12-10 01:06:54 +0530

    collection added

--- corejava/src/com/java/corejava/practice/collections.java
+++ corejava/src/com/java/corejava/practice/collections.java
@@ -1,11 +1,20 @@
 package com.java.corejava.practice;
 
+import java.util.Map;
+
 /**
  * @author Syam Dinesh
  */
 public class collections {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("this is test class");
+		
+		Map<String, Integer> map=null;
 	}
 }
