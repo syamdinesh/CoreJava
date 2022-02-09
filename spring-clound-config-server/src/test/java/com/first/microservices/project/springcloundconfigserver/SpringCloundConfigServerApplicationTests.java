cb1237f47ae6cdc1e8a2214a630db28969a8e33b
syamd <syamdinesh@gmail.com>
2022-02-09 18:59:36 +0530

    spring cloud first project

---
+++ spring-clound-config-server/src/test/java/com/first/microservices/project/springcloundconfigserver/SpringCloundConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.microservices.project.springcloundconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloundConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
